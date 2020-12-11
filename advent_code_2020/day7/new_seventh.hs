d46967f47a830975488e4f5f67cbbdcafe3743d0
Luka <elukej@gmail.com>
2020-12-11 20:30:18 +0100

    double speed AoC day7

--- advent_code_2020/day7/new_seventh.hs
+++ advent_code_2020/day7/new_seventh.hs
@@ -1,45 +1,42 @@
 import Control.Monad
 import Data.List
 
+filterBlank :: String -> String
 filterBlank "" = []
 filterBlank string = dropWhile (== ' ') string
 
+delim2 :: String -> String -> [String]
 delim2 _ [] = []
 delim2 charSet string = filter (/= "") $ [takeWhile (\x -> not (x `elem` charSet)) string] ++ 
                         delim2 charSet ((filterBlank . drop 1) $ (dropWhile (\x -> not (x `elem` charSet)) string))
 
-{- mislio sam da ce mi trebati ali ne. Ostavljam je radi eventualnog kopiranja kasnijih dana
-replace _ _ "" = ""
-replace c1 word (x:string) 
-                       | x == c1    = word ++ replace c1 c2 string
-                       | otherwise  = x:replace c1 c2 string
--}
---uzima ostatak stringa posle zadatog stringa
+afterWord :: String -> String -> String
 afterWord _ [] = []
 afterWord word (x:string)
                        | take (length word) (x:string) == word  = drop (length word) string
                        | otherwise                              = afterWord word string 
 
+beforeWord :: String -> String -> String
 beforeWord _ [] = []
 beforeWord word (x:string)
                        | take (length word) (x:string) == word  = []
                        | otherwise                              = x:beforeWord word string 
 
+foundMatch :: String -> String -> Bool
 foundMatch _ "" = False
 foundMatch word (x:string)
                        | take (length word) (x:string) == word   = True
                        | length string < length word             = False
                        | otherwise                               = foundMatch word string
 
+outerBags :: String -> [String] -> [String]
 outerBags _ [] = [""]
 outerBags bag fileString = fmap (beforeWord " bags") $ filter (foundMatch bag . afterWord "contain") fileString
 
-stackList acc [] _  = acc
-stackList acc (color:list) fileString = stackList (acc ++ (outerBags color fileString)) 
-                                                  (list ++ (filter (\x -> not $ x `elem` acc) $ outerBags color fileString))
-                                                  fileString 
-
-
+stackList ::  [String] -> [String] -> [String] -> [String]
+stackList acc [] _  = []
+stackList acc (color:list) fileString = bagsOut ++ (stackList (bagsOut ++ acc) ((bagsOut \\ acc) ++ list) fileString) 
+                                          where bagsOut = outerBags color fileString
 
 findBag _ [] = []
 findBag [] _ = []
@@ -59,8 +56,7 @@ stackListMod color fileString
 main = do
        fs <- readFile "input_day7.txt"
        let fileAsList = delim2 "\n" fs
-       return ()
-       print $ length . nub $ stackList [] ["shiny gold"] fileAsList 
+       print $ length . nub $ stackList [""] ["shiny gold"] fileAsList
        print $ stackListMod "shiny gold" fileAsList
 
 
