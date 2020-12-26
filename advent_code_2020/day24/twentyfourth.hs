1213df61aa9672a391c4c912e80ae126a6610000
Bludni <elukej@gmail.com>
2020-12-26 18:34:04 +0100

    Small improvements to day24 AoC

--- advent_code_2020/day24/twentyfourth.hs
+++ advent_code_2020/day24/twentyfourth.hs
@@ -2,13 +2,12 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as IOT
 import qualified Data.Map.Strict as Map
 import Data.List
+import Data.Bits ((.&.))
 
 main = do file <- fmap (T.unpack) . filter (/= T.pack "") . T.split (== '\n') <$> IOT.readFile "input_day24.txt"
           let tileMap = Map.fromListWith (+) . fmap (parse) $ file
           print $ foldl' (\acc x -> acc + (x `rem` 2)) 0 . Map.elems $ tileMap
-          print $ length . Map.keys . dayChange 100 $ Map.filter (\x -> x `rem` 2 == 1) tileMap
-
---nije 13366
+          print $ length . Map.keys . dayChange 100 $ Map.filter (\x -> toEnum $ x .&. 1) tileMap
 
 parse string = parse' ((0,0),1) string 
       where parse' res1 [] = res1 
@@ -21,18 +20,17 @@ parse string = parse' ((0,0),1) string
 
 prepTiles tMap = prepTiles' (Map.keys tMap) tMap
            where prepTiles' [] tMap1 = tMap1
-                 prepTiles' ((x,y):list) tMap1 = prepTiles' list $ tMap1 `Map.union` (Map.fromList $ zipWith (\x (Just y) -> (x,y)) forMap vList) 
-                  where forMap =[(x+1,y),(x-1,y),(x+0.5,y+1),(x-0.5,y-1),(x+0.5,y-1),(x-0.5,y+1)]
-                        vList = fmap (\x -> if Map.member x tMap1 then Map.lookup x tMap1 else Just 0) forMap 
+                 prepTiles' ((x,y):list) tMap1 = prepTiles' list $ tMap1 `Map.union` (Map.fromList forMap) 
+                  where forMap =[((x+1,y),0),((x-1,y),0),((x+0.5,y+1),0),((x-0.5,y-1),0),((x+0.5,y-1),0),((x-0.5,y+1),0)]
 
 travTiles tMap = travTiles' (Map.keys tMap) tMap 
       where travTiles' [] tMap1 = tMap1
-            travTiles' ((x,y):list1) tMap1 | (vref `rem` 2 == 0) && nrBlack /= 2 || (vref `rem` 2 == 1) && (not $ nrBlack `elem` [1,2]) = travTiles' list1 $ Map.delete (x,y) tMap1
-                                           | otherwise = travTiles' list1 $ Map.insertWith (+) (x,y) (fromEnum (vref `rem` 2 == 0)) tMap1          
+            travTiles' ((x,y):list1) tMap1 | (vref .&. 1 == 0) && nrBlack /= 2 || (vref .&. 1 == 1) && (not $ nrBlack `elem` [1,2]) = travTiles' list1 $ Map.delete (x,y) tMap1
+                                           | otherwise = travTiles' list1 $ Map.insertWith (+) (x,y) (fromEnum (vref .&. 1 == 0)) tMap1
                                                   where forMap = [(x+1,y),(x-1,y),(x+0.5,y+1),(x-0.5,y-1),(x+0.5,y-1),(x-0.5,y+1)]
                                                         vList = fmap (\x -> if Map.member x tMap then Map.lookup x tMap else Just 0) forMap 
                                                         Just vref = Map.lookup (x,y) tMap1
-                                                        nrBlack = foldl' (\acc (Just x) -> acc + (x `rem` 2)) 0 vList
+                                                        nrBlack = foldl' (\acc (Just x) -> acc + (x .&. 1)) 0 vList
 
 dayChange :: Int -> Map.Map (Float,Int) Int -> Map.Map (Float,Int) Int
 dayChange 0 tMap = tMap
